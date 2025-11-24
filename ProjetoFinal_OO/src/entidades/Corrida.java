cb5346a735d884ce5cdba6f0eee908f3c5b679ae
LuizXIV <luizfelipemcg@gmail.com>
2025-11-24 14:18:01 -0300

    Corrida.java

--- ProjetoFinal_OO/src/entidades/Corrida.java
+++ ProjetoFinal_OO/src/entidades/Corrida.java
@@ -1,12 +1,24 @@
 package entidades;
 import servicos.MetodoPagamento;
 
-public class Corrida {
-	private String id;
-    private Passageiro passageiro;
-    private Motorista motorista;
-    private StatusCorrida status;
-    private double precoFinal;
-    private MetodoPagamento metodoPagamento;
-    private Categoria categoria;
+
+public class Corrida  {
+		private String id;
+	    private Passageiro passageiro;
+	    private Motorista motorista;
+	    private StatusCorrida status;
+	    private double precoFinal;
+	    private MetodoPagamento metodoPagamento;
+	    private Categoria categoria;
+	    private Local partida;
+	    private Local destino;
+	    private double distancia;
+	    private double precoEstimado;
+	    
+	    
+	     public double calcularPrecoEstimado() {
+	    	
+	    }
+	    
+
 }
