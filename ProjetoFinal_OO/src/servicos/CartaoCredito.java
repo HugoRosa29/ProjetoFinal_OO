541035243076fb01749dbb142d139cf334a3664c
LuizXIV <luizfelipemcg@gmail.com>
2025-11-24 14:24:18 -0300

    CartaoCredito.java

--- ProjetoFinal_OO/src/servicos/CartaoCredito.java
+++ ProjetoFinal_OO/src/servicos/CartaoCredito.java
@@ -1,11 +1,38 @@
 package servicos;
 
+import java.util.Date;
+
 public class CartaoCredito implements MetodoPagamento{
-	private String numeroCartao;
+	private String numeroCartao, nome, titular, cvv;
+	Date validade;
 	
-	public CartaoCredito(String numeroCartao) {
+	
+	
+	
+	public CartaoCredito(String numeroCartao, String nome, String titular, String cvv, Date validade) {
 		this.setNumeroCartao(numeroCartao);
-	}
+		this.nome = nome;
+		this.titular = titular;
+		this.cvv = cvv;
+		this.validade = validade;
+		}
+	
+	
+	public String getNome() {return nome;}
+	
+	public String getTitular() {return titular;}
+	
+	public String getCvv() {return cvv;}
+	
+	public Date getValidade() {return validade;}
+	
+	
+	public void setNome() {}
+	public void setTitular() {}
+	public void setCvv() {}
+	public void setValidade() {}
+	
+	
 	
 	@Override
 	public boolean processarPagamento(double valor) {
