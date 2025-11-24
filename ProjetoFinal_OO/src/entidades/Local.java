b9c8f8009a7173222fb26b77c8142f74bd2db7dd
LuizXIV <luizfelipemcg@gmail.com>
2025-11-24 14:08:12 -0300

    Local.java

---
+++ ProjetoFinal_OO/src/entidades/Local.java
@@ -0,0 +1,23 @@
+package entidades;
+
+public class Local {
+protected Double latitude;
+protected Double longitude;
+protected String endereco;
+
+public Local(Double latitude, Double longitude, String endereco) {
+	this.endereco = endereco;
+	this.latitude = latitude;
+	this.longitude = longitude;
+	
+}
+
+
+
+public double calcularDistancia() {
+	
+}
+
+
+
+}
